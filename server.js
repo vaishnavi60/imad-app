c9a640385a2205010fc3fe494834c966ec573c54
vaishnavi60 <31343153+vaishnavi60@users.noreply.github.com>
2018-03-29 11:29:55 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -6,16 +6,17 @@ var app = express();
 app.use(morgan('combined'));
 
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+     res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+  
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
-
 app.get('/article-one',function(req,res){
-    res.send('article one requested will be server here');
+     res.sendFile(path.join(__dirname, 'ui', 'Article-one.html'));
 });
+
 app.get('/article-two',function(req,res){
     res.send('article two requested will be server here');
 });
